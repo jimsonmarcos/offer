0b9d806341ada2a29d1ee67c35a3ce2b1ae3bfbc
unknown <jimson.d.marcos@gmail.com>
2023-11-17 03:19:57 +0800

    use image files instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex flex-1 justify-center min-h-screen bg-[#2B2D42] relative overflow-hidden">
-      <div className="absolute w-[720px] h-[720px] rounded-[720px] bg-[#0083FE] top-[-500px] left-[13%] blur-[350px] flex justify-center z-10">
+      <Image src="/radial-top-left.png" className="absolute top-0 left-0 z-10" width={1610} height={962} alt="" />
+      <Image src="/radial-bottom-right.png" className="absolute bottom-0 right-0 z-10" width={948} height={1263} alt="" />
+      {/* <div className="absolute w-[720px] h-[720px] rounded-[720px] bg-[#0083FE] top-[-500px] left-[13%] blur-[350px] flex justify-center z-10">
         <div className="w-[500px] h-[500px] rounded-[500px] bg-[#0083FE] opacity-50 self-center blur-[150px] z-10">
         </div>
       </div>
@@ -11,7 +13,7 @@ export default function Home() {
       <div className="absolute w-[720px] h-[720px] rounded-[720px] bg-[#6D6F84] bottom-[57px] right-[-400px] blur-[350px] flex justify-center z-10">
         <div className="w-[500px] h-[500px] rounded-[500px] bg-[#6D6F84] opacity-50 self-center blur-[150px] z-[15px]">
         </div>
-      </div>
+      </div> */}
 
       <div className="h-[618px] w-[1200px] bg-no-repeat flex justify-end self-center relative z-20" style={{ backgroundImage: "url('/image.png')" }}>
         <div className="w-[995px] h-[511px] self-center">
